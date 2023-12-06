9aadcb52bd3b36627e607d9bd99021ba7e2fea2f
Takae <93442902+takaesa@users.noreply.github.com>
2023-12-06 17:25:35 +0700

    finish temporary header and footer

--- src/components/layouts/default/Footer/index.tsx
+++ src/components/layouts/default/Footer/index.tsx
@@ -1,11 +1,30 @@
 export interface IFooterProps {
     title?: string;
 }
-
+import { IoLogoTwitter } from "react-icons/io5";
+import { PiInstagramLogoFill } from "react-icons/pi";
+import { IoLogoGithub } from "react-icons/io";
+import Link from "next/link";
 const Footer = () => {
+
     return (
         <>
-            <h1 className='text-danger text-center'>Footer</h1>
+        <div className="flex main-colored p-1">
+            <div className='ms-5 mb-2 mt-2 size-1em'>
+                {/* <MDBIcon fab icon="twitter" /> */}
+                <IoLogoTwitter />
+                <IoLogoGithub />
+                <PiInstagramLogoFill />
+            </div>
+                
+            <div className='text-left  main-colored ms-5 mb-2 d-flex' >
+            © 2023 TOKI. All Rights Reserved. 
+            <div> <Link href='/' className="ms-2 me-1 text-white"> Terms of Service </Link></div>
+             | 
+            <div><Link href='/' className="ms-1 text-white"> Privacy Policy </Link></div>
+
+            </div>
+        </div>   
         </>
     );
 };
--- src/components/layouts/default/Header/index.tsx
+++ src/components/layouts/default/Header/index.tsx
@@ -1,12 +1,44 @@
 export interface IHeaderProps {
     title?: string;
 }
+import Link from "next/link";
+import { IoIosNotifications } from "react-icons/io";
+import { IoIosHelpCircleOutline } from "react-icons/io";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { IoIosSearch } from "react-icons/io";
+
 
 const Header = (props: IHeaderProps) => {
     const { title } = props;
     return (
         <>
-            <h1 className='text-danger text-center'>{title || 'Header'}</h1>
+            
+                <div className='d-flex main-colored p-1 text-black'>
+
+                <button type="button" className='btn ms-5 text-white ' >Seller Center</button>
+                <button type="button" className='btn ms-auto me-5 text-white'><IoIosNotifications/> Notification</button>
+                <button type="button" className='btn me-5 text-white'><IoIosHelpCircleOutline/> Help</button>
+                <button type="button" className='btn me-1 text-white'> Login </button>
+                
+                <button type="button" className='btn me-5 text-white' > Sign Up </button>
+            </div>
+
+            <div className="d-flex " >
+                <img src="src/app/assets/images/Logo.png" alt="abc" />
+                <div className="text-danger main-colored position-absolute start-50 translate-middle mt-3">
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                </div>
+                <div className="ms-auto me-5 p-3">
+                    <button type="button" className="btn  me-5  "> <IoIosSearch /> </button>
+                    <button type="button" className="btn me-5 position-relative"> <AiOutlineShoppingCart />
+                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary">+99 
+                        <span className="visually-hidden">unread messages
+                        </span></span> 
+                    </button>
+                </div>
+                
+            </div>
+                                
         </>
     );
 };
