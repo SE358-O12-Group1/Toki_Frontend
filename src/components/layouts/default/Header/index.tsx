fa699583ec77b2fa13394740f94a054d535fc893
Takae <93442902+takaesa@users.noreply.github.com>
2023-12-08 23:22:01 +0700

    create container for category

--- src/components/layouts/default/Header/index.tsx
+++ src/components/layouts/default/Header/index.tsx
@@ -16,7 +16,7 @@ const Header = (props: IHeaderProps) => {
             
                 <div className='d-flex main-colored p-1 text-black'>
 
-                <button type="button" className='btn offset-1 text-white ' >Seller Center</button>
+                <button type="button" className='btn offset-1 text-white' >Seller Center</button>
                 <button type="button" className='btn ms-auto me-5 text-white'> <img src={Notification.src} /> Notification</button>
                 <button type="button" className='btn me-5 text-white'> <img src={Question.src} /> Help</button>
                 <button type="button" className='btn me-1 text-white'> Login </button>
@@ -25,12 +25,14 @@ const Header = (props: IHeaderProps) => {
             </div>
 
             <div className="d-flex p-3" >
-                <img src={Logo.src} className=" offset-1"/>
+                <div className="offset-1">
+                    <img className="ms-5 " src={Logo.src} />
+                </div>
                 <div className="text-danger main-colored position-absolute start-50 translate-middle mt-3">
                     AAAAAAAAAAAAAAAAAAAAAAAAAAAA
                 </div>
                 <div className="ms-auto me-5">
-                    <button type="button" className="btn  me-4 mt-1 "> <img src={Search.src} /> </button>
+                    <button type="button" className="btn me-4 mt-1 btn-sm"> <img src={Search.src} /> </button>
                     <button type="button" className="btn ms-5 me-5 mt-1 position-relative"> <img src={Cart.src} />
                         <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">+99 
                         <span className="visually-hidden">unread messages
--- src/components/pages/LandingPage/index.tsx
+++ src/components/pages/LandingPage/index.tsx
@@ -1,10 +1,34 @@
 
 export interface ILandingPageProps {}
-
+import menClothes from "src/app/assets/images/menClothes.png"
 export default function LandingPage(props: ILandingPageProps) {
     return (
         <>
-            <div className="" ></div>
+        <div className="offset-1"> 
+        <table className="table table-bordered table-hover main-colored ">
+            <div className="col">
+                <thead>
+                    <tr>
+                        <th scope="col">CATEGORY</th>
+                    </tr>
+                </thead>
+                <tbody className="">
+                    <tr>
+                        <th scope="row"> <img src={menClothes.src}/> </th>
+                    </tr>
+                    <tr>1</tr>
+                    <tr>1</tr>
+                    <tr>1</tr>
+                    <tr>1</tr>
+                    <tr>1</tr>
+                    <tr>1</tr>
+                </tbody>
+            </div>
+            <div className="col"></div>
+            
+            
+        </table></div>
+        
             <h1>Landing Page</h1>
         </>
     );
