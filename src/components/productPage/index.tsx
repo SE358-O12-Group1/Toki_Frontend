66dfd6cf63134d9d212ae37643f761f4bf01fded
thanhLoc1087 <thanhlocne246@gmail.com>
2023-12-13 18:56:17 +0700

    TOKI-58 product page layout

---
+++ src/components/productPage/index.tsx
@@ -0,0 +1,375 @@
+'use client';
+
+import DOMPurify from 'isomorphic-dompurify';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import Product from '../landing/product';
+import {
+    formatCurrency,
+    formatNumberToSocialStyle,
+    rateSale
+} from '@/utils/utils';
+import { IProduct, mockProduct } from './mockData';
+import QuantityController from '../common/QuantityController';
+
+export interface ProductDetailProps {
+    productId: string | number;
+}
+export default function ProductDetailPage(props: ProductDetailProps) {
+    const { productId } = props;
+    const [buyCount, setBuyCount] = useState(1);
+
+    const [currentIndexImages, setCurrentIndexImages] = useState([0, 5]);
+    const [activeImage, setActiveImage] = useState('');
+    const product = mockProduct;
+    const imageRef = useRef<HTMLImageElement>(null);
+    const currentImages = useMemo(
+        () => (product ? product?.images.slice(...currentIndexImages) : []),
+        [product, currentIndexImages]
+    );
+
+    // const queryConfig: ProductListConfig = {
+    //     limit: '20',
+    //     page: '1',
+    //     category: product?.category._id
+    // };
+
+    // const { data: productsData } = useQuery({
+    //     queryKey: ['products', queryConfig],
+    //     queryFn: () => {
+    //         return productApi.getProducts(queryConfig);
+    //     },
+    //     enabled: Boolean(product),
+    //     staleTime: 3 * 60 * 1000
+    // });
+
+    // const addToCartMutation = useMutation(purchaseApi.addToCart);
+
+    useEffect(() => {
+        if (product && product.images.length > 0) {
+            setActiveImage(product.images[0]);
+        }
+    }, [product]);
+
+    const next = () => {
+        if (currentIndexImages[1] < (product as IProduct).images.length) {
+            setCurrentIndexImages((prev) => [prev[0] + 1, prev[1] + 1]);
+        }
+    };
+
+    const prev = () => {
+        if (currentIndexImages[0] > 0) {
+            setCurrentIndexImages((prev) => [prev[0] - 1, prev[1] - 1]);
+        }
+    };
+
+    const chooseActive = (img: string) => {
+        setActiveImage(img);
+    };
+
+    const handleZoom = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        const rect = event.currentTarget.getBoundingClientRect();
+        const image = imageRef.current as HTMLImageElement;
+        const { naturalHeight, naturalWidth } = image;
+        // Cách 1: Lấy offsetX, offsetY đơn giản khi chúng ta đã xử lý được bubble event
+        // Dùng css pointer-events-none
+        // const { offsetX, offsetY } = event.nativeEvent;
+
+        // Cách 2: Lấy offsetX, offsetY khi chúng ta không xử lý được bubble event
+        const offsetX = event.pageX - (rect.x + window.scrollX);
+        const offsetY = event.pageY - (rect.y + window.scrollY);
+
+        const top = offsetY * (1 - naturalHeight / rect.height);
+        const left = offsetX * (1 - naturalWidth / rect.width);
+        image.style.width = naturalWidth + 'px';
+        image.style.height = naturalHeight + 'px';
+        image.style.maxWidth = 'unset';
+        image.style.top = top + 'px';
+        image.style.left = left + 'px';
+    };
+
+    const handleRemoveZoom = () => {
+        imageRef.current?.removeAttribute('style');
+    };
+
+    const handleBuyCountChange = (value: number) => {
+        setBuyCount(value);
+    };
+
+    const addToCart = () => {
+        // addToCartMutation.mutate(
+        //     { buy_count: buyCount, product_id: product?._id as string },
+        //     {
+        //         onSuccess: (data) => {
+        //             queryClient.invalidateQueries({
+        //                 queryKey: [
+        //                     'purchases',
+        //                     { status: purchasesStatus.inCart }
+        //                 ]
+        //             });
+        //             toast.success(data.data.message, {
+        //                 position: 'top-center',
+        //                 autoClose: 1500
+        //             });
+        //         }
+        //     }
+        // );
+    };
+
+    const buyNow = async () => {};
+
+    if (!product) {
+        return null;
+    }
+
+    return (
+        <>
+            <div className='bg-gray-200 py-6'>
+                <div className='container'>
+                    <div className='bg-white p-4 shadow'>
+                        <div className='grid grid-cols-12 gap-9'>
+                            <div className='col-span-5'>
+                                <div
+                                    className='relative w-full cursor-zoom-in overflow-hidden pt-[100%] shadow'
+                                    onMouseMove={handleZoom}
+                                    onMouseLeave={handleRemoveZoom}
+                                >
+                                    <img
+                                        src={activeImage}
+                                        alt={product.name}
+                                        className='absolute left-0 top-0 h-full w-full bg-white object-cover'
+                                        ref={imageRef}
+                                    />
+                                </div>
+                                <div className='relative mt-4 grid grid-cols-5 gap-1'>
+                                    <button
+                                        className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                                        onClick={prev}
+                                    >
+                                        <svg
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            fill='none'
+                                            viewBox='0 0 24 24'
+                                            strokeWidth={1.5}
+                                            stroke='currentColor'
+                                            className='h-5 w-5'
+                                        >
+                                            <path
+                                                strokeLinecap='round'
+                                                strokeLinejoin='round'
+                                                d='M15.75 19.5L8.25 12l7.5-7.5'
+                                            />
+                                        </svg>
+                                    </button>
+                                    {currentImages.map((img) => {
+                                        const isActive = img === activeImage;
+                                        return (
+                                            <div
+                                                className='relative w-full pt-[100%]'
+                                                key={img}
+                                                onMouseEnter={() =>
+                                                    chooseActive(img)
+                                                }
+                                            >
+                                                <img
+                                                    src={img}
+                                                    alt={product.name}
+                                                    className='absolute left-0 top-0 h-full w-full cursor-pointer bg-white object-cover'
+                                                />
+                                                {isActive && (
+                                                    <div className='border-orange absolute inset-0 border-2' />
+                                                )}
+                                            </div>
+                                        );
+                                    })}
+                                    <button
+                                        className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                                        onClick={next}
+                                    >
+                                        <svg
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            fill='none'
+                                            viewBox='0 0 24 24'
+                                            strokeWidth={1.5}
+                                            stroke='currentColor'
+                                            className='h-5 w-5'
+                                        >
+                                            <path
+                                                strokeLinecap='round'
+                                                strokeLinejoin='round'
+                                                d='M8.25 4.5l7.5 7.5-7.5 7.5'
+                                            />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                            <div className='col-span-7'>
+                                <h1 className='text-xl font-medium uppercase'>
+                                    {product.name}
+                                </h1>
+                                <div className='mt-8 flex items-center'>
+                                    {/* <div className='flex items-center'>
+                                        <span className='border-b-orange text-orange mr-1 border-b'>
+                                            {product.rating}
+                                        </span>
+                                        <ProductRating
+                                            rating={product.rating}
+                                            activeClassname='fill-orange text-orange h-4 w-4'
+                                            nonActiveClassname='fill-gray-300 text-gray-300 h-4 w-4'
+                                        />
+                                    </div> */}
+                                    <div className='mx-4 h-4 w-[1px] bg-gray-300'></div>
+                                    <div>
+                                        <span>
+                                            {formatNumberToSocialStyle(
+                                                product.sold_quantity
+                                            )}
+                                        </span>
+                                        <span className='ml-1 text-gray-500'>
+                                            {'product detail.sold'}
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div className='mt-8 flex items-center bg-gray-50 px-5 py-4'>
+                                    <div className='text-gray-500 line-through'>
+                                        ₫{formatCurrency(product.price)}
+                                    </div>
+                                    <div className='text-orange ml-3 text-3xl font-medium'>
+                                        ₫{formatCurrency(product.price)}
+                                    </div>
+                                    <div className='bg-orange ml-4 rounded-sm px-1 py-[2px] text-xs font-semibold uppercase text-white'>
+                                        {rateSale(product.price, product.price)}{' '}
+                                        {'product detail.off'}
+                                    </div>
+                                </div>
+
+                                <div className='mt-8 flex items-center'>
+                                    <div className='capitalize text-gray-500'>
+                                        {'product detail.quantity'}
+                                    </div>
+                                    <QuantityController
+                                        onDecrease={handleBuyCountChange}
+                                        onIncrease={handleBuyCountChange}
+                                        onType={handleBuyCountChange}
+                                        value={buyCount}
+                                        max={product.quantity}
+                                    />
+                                    <div className='ml-6 text-sm text-gray-500'>
+                                        {product.quantity}{' '}
+                                        {'product detail.pieces available'}
+                                    </div>
+                                </div>
+
+                                <div className='mt-8 flex items-center'>
+                                    <button
+                                        onClick={addToCart}
+                                        className='border-orange bg-orange/10 text-orange hover:bg-orange/5 flex h-12 items-center justify-center rounded-sm border px-5 capitalize shadow-sm'
+                                    >
+                                        <svg
+                                            enableBackground='new 0 0 15 15'
+                                            viewBox='0 0 15 15'
+                                            x={0}
+                                            y={0}
+                                            className='stroke-orange text-orange mr-[10px] h-5 w-5 fill-current'
+                                        >
+                                            <g>
+                                                <g>
+                                                    <polyline
+                                                        fill='none'
+                                                        points='.5 .5 2.7 .5 5.2 11 12.4 11 14.5 3.5 3.7 3.5'
+                                                        strokeLinecap='round'
+                                                        strokeLinejoin='round'
+                                                        strokeMiterlimit={10}
+                                                    />
+                                                    <circle
+                                                        cx={6}
+                                                        cy='13.5'
+                                                        r={1}
+                                                        stroke='none'
+                                                    />
+                                                    <circle
+                                                        cx='11.5'
+                                                        cy='13.5'
+                                                        r={1}
+                                                        stroke='none'
+                                                    />
+                                                </g>
+                                                <line
+                                                    fill='none'
+                                                    strokeLinecap='round'
+                                                    strokeMiterlimit={10}
+                                                    x1='7.5'
+                                                    x2='10.5'
+                                                    y1={7}
+                                                    y2={7}
+                                                />
+                                                <line
+                                                    fill='none'
+                                                    strokeLinecap='round'
+                                                    strokeMiterlimit={10}
+                                                    x1={9}
+                                                    x2={9}
+                                                    y1='8.5'
+                                                    y2='5.5'
+                                                />
+                                            </g>
+                                        </svg>
+                                        {'product detail.add to cart'}
+                                    </button>
+                                    <button
+                                        className='fkex bg-orange hover:bg-orange/90 ml-4 h-12 min-w-[5rem] items-center justify-center rounded-sm px-5 capitalize text-white shadow-sm outline-none'
+                                        onClick={buyNow}
+                                    >
+                                        {'product detail.buy now'}
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='mt-8'>
+                    <div className='container'>
+                        <div className='mt-8 bg-white p-4 shadow'>
+                            <div className='rounded bg-gray-50 p-4 text-lg capitalize text-slate-700'>
+                                {'product detail.product description'}
+                            </div>
+                            <div className='mx-4 mb-4 mt-12 text-sm leading-loose'>
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html: DOMPurify.sanitize(
+                                            product.description
+                                        )
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='mt-8'>
+                    <div className='container'>
+                        <div className='uppercase text-gray-400'>
+                            {'product detail.you may also like'}
+                        </div>
+                        {product.relatedProducts && (
+                            <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
+                                {product.relatedProducts.map((product) => (
+                                    <div
+                                        className='col-span-1'
+                                        key={product._id}
+                                    >
+                                        {/* <Product product={product} /> */}
+                                        <div>{product.name}</div>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
