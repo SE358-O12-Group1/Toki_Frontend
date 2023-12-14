0353dbe5253ccbb7c1e508e86668ac6173759cd9
plongtran239 <plongtran239@gmail.com>
2023-12-14 23:22:59 +0700

    TOKI-79 Integrate Detail Product Page With Id

--- src/redux/slices/auth.slice.ts
+++ src/redux/slices/auth.slice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 import AuthType, { initialAuth } from '@/types/AuthType';
+import { setAuthDataToLS } from '@/utils/auth';
 
 const authSlice = createSlice({
     name: 'auth',
@@ -10,28 +11,14 @@ const authSlice = createSlice({
             state.user = action.payload.user;
             state.accessToken = action.payload.accessToken;
             state.refreshToken = action.payload.refreshToken;
-            localStorage.setItem(
-                'auth_data',
-                JSON.stringify({
-                    user: action.payload.user,
-                    accessToken: action.payload.accessToken,
-                    refreshToken: action.payload.refreshToken
-                })
-            );
+            setAuthDataToLS(action.payload);
         },
 
         login: (state, action: PayloadAction<AuthType>) => {
             state.user = action.payload.user;
             state.accessToken = action.payload.accessToken;
             state.refreshToken = action.payload.refreshToken;
-            localStorage.setItem(
-                'auth_data',
-                JSON.stringify({
-                    user: action.payload.user,
-                    accessToken: action.payload.accessToken,
-                    refreshToken: action.payload.refreshToken
-                })
-            );
+            setAuthDataToLS(action.payload);
         }
     }
 });
