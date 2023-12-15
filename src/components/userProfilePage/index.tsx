d4b34cd75f198bf0a79a461818099963c0fc2074
Takae <93442902+takaesa@users.noreply.github.com>
2023-12-16 05:42:46 +0700

    complete Toki-68-Implement UI for user profile page

--- src/components/userProfilePage/index.tsx
+++ src/components/userProfilePage/index.tsx
@@ -5,6 +5,9 @@ import Header from '@/components/common/MainLayout/Header';
 import Circle from '/public/assets/images/userprofilecircle.png';
 import profile from '/public/assets/images/profile.png';
 import order from '/public/assets/images/orderbutton.png';
+import bigCircle from '/public/assets/images/bigprofilecircle.png';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 export interface IUserProfilePage {}
 
@@ -12,23 +15,23 @@ export default function UserProfilePage(props: IUserProfilePage) {
     return (
         <>
             <Header></Header>
-            <div className='row mt-4' style={{ backgroundColor: '#E2E2E2' }}>
-                <div className='row d-flex mt-5'>
+            <div className='row mt-3' style={{ backgroundColor: '#E2E2E2' }}>
+                <div className='row' style={{ marginTop: 30 }}>
                     <div
                         className='col d-flex '
                         style={{ marginInlineStart: 170 }}
                     >
                         <div
-                            className='col-2'
+                            className='col-2 '
                             style={{
                                 display: 'flex',
                                 flexDirection: 'column',
                                 alignItems: 'center',
-                                justifyContent: 'center'
+                                marginTop: 40
                             }}
                         >
                             <img
-                                className='mb-5 mt-3'
+                                className='mb-5'
                                 style={{ scale: 1.3 }}
                                 src={Circle.src}
                             />
@@ -44,17 +47,18 @@ export default function UserProfilePage(props: IUserProfilePage) {
                         </div>
                         <div className='col ms-3'>
                             <div
-                                className='text  mt-3'
+                                className='text'
                                 style={{
                                     fontSize: 26,
                                     color: '#00ADB5',
-                                    fontWeight: 700
+                                    fontWeight: 700,
+                                    marginTop: 40,
+                                    display: 'flex'
                                 }}
                             >
                                 Username
                             </div>
                             <div
-                                className='col'
                                 style={{
                                     fontSize: 18,
                                     marginTop: 52
@@ -99,41 +103,148 @@ export default function UserProfilePage(props: IUserProfilePage) {
                     </div>
 
                     <div
-                        className='col-8 ms-auto'
+                        className='col-8'
                         style={{
-                            marginInlineEnd: 120,
+                            marginInlineEnd: 110,
                             backgroundColor: '#ffffff',
                             borderRadius: 15
                         }}
                     >
-                        <div className='row'>
+                        <div className='row '>
+                            <div className='row'>
+                                <div
+                                    className='text mt-3'
+                                    style={{
+                                        fontSize: 35,
+                                        fontWeight: 700,
+                                        marginInlineStart: 20
+                                    }}
+                                >
+                                    User profile
+                                </div>
+                                <div>
+                                    {' '}
+                                    <hr
+                                        style={{
+                                            display: 'block',
+                                            border: '1px solid #000',
+                                            margin: 20
+                                        }}
+                                    ></hr>
+                                </div>
+                            </div>
                             <div
-                                className='text  mt-3'
+                                className='col-2    '
                                 style={{
-                                    fontSize: 30,
-                                    fontWeight: 700,
-                                    marginInlineStart: 20
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    marginLeft: 80,
+                                    marginTop: 75
                                 }}
                             >
-                                User profile
-                            </div>
-                            <div>
-                                {' '}
-                                <hr
+                                <img className='mb-4' src={bigCircle.src} />
+                                <Button
+                                    size='large'
+                                    variant='outlined'
                                     style={{
-                                        display: 'block',
-                                        height: 1,
-                                        border: 0,
-                                        borderTop: 1,
-                                        borderTopColor: '#D9D9D9',
-                                        margin: 1,
-                                        padding: 0
+                                        textTransform: 'none',
+                                        color: 'black',
+                                        borderColor: '#D2D1D1',
+                                        marginTop: 10
                                     }}
-                                ></hr>
+                                >
+                                    New photo
+                                </Button>
+                            </div>
+                            <div
+                                className='col-2'
+                                style={{
+                                    fontWeight: 600,
+                                    textAlign: 'right',
+                                    fontSize: 20,
+                                    marginTop: 30
+                                }}
+                            >
+                                <div className='row d-flex space-y-7'>
+                                    <div className='mb-4 mt-4'>Username</div>
+                                    <div className='mb-4'>Name</div>
+                                    <div className='mb-4'>Email</div>
+                                    <div className='mb-4'>Phone Number</div>
+                                </div>
+                            </div>
+                            <div className='col'>
+                                <div
+                                    className='row mb-5 me-4'
+                                    style={{ marginTop: 25 }}
+                                >
+                                    <TextField
+                                        size='medium'
+                                        style={{
+                                            width: '10',
+                                            marginTop: 15,
+                                            fontSize: 20,
+                                            textTransform: 'none'
+                                        }}
+                                    >
+                                        {' '}
+                                    </TextField>
+                                    <TextField
+                                        size='medium'
+                                        style={{
+                                            width: '100',
+                                            marginTop: 25
+                                        }}
+                                    >
+                                        {' '}
+                                    </TextField>
+                                    <TextField
+                                        size='medium'
+                                        style={{
+                                            width: '100',
+                                            marginTop: 25
+                                        }}
+                                    >
+                                        {' '}
+                                    </TextField>
+                                    <TextField
+                                        size='medium'
+                                        style={{
+                                            width: '100',
+                                            marginTop: 25
+                                        }}
+                                    >
+                                        {' '}
+                                    </TextField>
+                                </div>
+                                <div
+                                    className='container'
+                                    style={{
+                                        textAlign: 'right'
+                                    }}
+                                >
+                                    <Button
+                                        size='large'
+                                        variant='outlined'
+                                        style={{
+                                            background: '#00ADB5',
+                                            color: 'white',
+                                            minWidth: 280,
+                                            alignItems: 'center',
+                                            marginRight: 22,
+                                            textTransform: 'none',
+                                            marginBottom: 50,
+                                            fontSize: 20
+                                        }}
+                                    >
+                                        Save change
+                                    </Button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div className='row' style={{ minHeight: 55 }}></div>
             </div>
         </>
     );
