6581a68a1007ce443b3f1577edde20137431b48e
plongtran239 <plongtran239@gmail.com>
2023-12-20 11:21:29 +0700

    TOKI-69 Integrate Landing Page with API

--- src/app/(page)/(auth)/signup/page.tsx
+++ src/app/(page)/(auth)/signup/page.tsx
@@ -1,6 +1,14 @@
+'use client';
+
+import { useEffect } from 'react';
+
 import SignupForm from '@/components/auth/SignupForm';
 
 const SignUp = () => {
+    useEffect(() => {
+        document.title = 'TOKI | Sign Up';
+    }, []);
+
     return <SignupForm />;
 };
 
