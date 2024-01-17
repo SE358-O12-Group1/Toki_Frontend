6f846698e1225d65bb6cd56c50889a8d56a075d3
ThanhLoc <thanhlocne246@gmail.com>
2024-01-17 22:38:28 +0700

    TOKI-93 wip

--- src/components/common/ConfirmationModal.tsx
+++ src/components/common/ConfirmationModal.tsx
@@ -14,11 +14,50 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
     if (!isOpen) return null;
 
     return (
-        <div className='modal'>
-            <div className='modal-content'>
-                <p>Are you sure you want to proceed?</p>
-                <button onClick={onConfirm}>Yes</button>
-                <button onClick={onClose}>No</button>
+        <div
+            className='modal fade'
+            id='exampleModal'
+            tabIndex={-1}
+            role='dialog'
+            aria-labelledby='exampleModalLabel'
+            aria-hidden='true'
+        >
+            <div className='modal-dialog' role='document'>
+                <div className='modal-content'>
+                    <div className='modal-header'>
+                        <h5 className='modal-title' id='exampleModalLabel'>
+                            Confirm
+                        </h5>
+                        <button
+                            type='button'
+                            className='close'
+                            data-dismiss='modal'
+                            aria-label='Close'
+                        >
+                            <span aria-hidden='true'>&times;</span>
+                        </button>
+                    </div>
+                    <div className='modal-body'>
+                        {'Delete items?\nThis action cannot be revert.'}
+                    </div>
+                    <div className='modal-footer'>
+                        <button
+                            type='button'
+                            className='btn btn-secondary'
+                            data-dismiss='modal'
+                            onClick={onClose}
+                        >
+                            Close
+                        </button>
+                        <button
+                            type='button'
+                            className='btn btn-main'
+                            onClick={onConfirm}
+                        >
+                            Save changes
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
